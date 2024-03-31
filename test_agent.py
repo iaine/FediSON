6cdc233f4a4c03224edad993e9eb6c5f0edc31b0
iaine <iain.emsley@kellogg.ox.ac.uk>
2024-03-31 21:05:40 +0100

    Added tests

--- test_agent.py
+++ test_agent.py
@@ -1,9 +1,35 @@
-import agent
+from agent import createAgent
 import os
 
 def test_file_creation():
     name = "test"
     port = 4557
     blocks = []
-    agent.create_agent(name, port, blocks)
+    fh = open('server_template.ck', 'r')
+    template = fh.read()
+    fh.close()   
+    createAgent(port).create_agent(template, name,blocks)
     assert(os.path.exists(os.getcwd() + '/agents/' + name + '.ck'))
+    with open(os.getcwd() + '/agents/' + name + '.ck', 'r') as fh:
+        data = fh.read()
+    assert("int port <= " + port in data)
+    assert("stirng name <= " + name in data)
+    os.remove(os.getcwd() + '/agents/' + name + '.ck')
+
+def test_two_file_creation():
+    name = ["test", "baz"]
+    port = 4557
+    blocks = []
+    fh = open('server_template.ck', 'r')
+    template = fh.read()
+    fh.close()   
+    createAgent(port).create_agent(template, name,blocks)
+    assert(os.path.exists(os.getcwd() + '/agents/test.ck'))
+    assert(os.path.exists(os.getcwd() + '/agents/baz.ck'))
+    os.remove(os.getcwd() + '/agents/test.ck')
+    os.remove(os.getcwd() + '/agents/baz.ck')
+
+def test_create_command():
+    cmd = createAgent().run_command(['foo', 'bar', 'baz'])
+    assert(cmd == " foo.ck bar.ck baz.ck")
+    assert(cmd != " foo.ck bar.ck baz.ck moe.ck")
