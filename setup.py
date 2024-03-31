360faf223a9a079e023f64faf9cf8c8d4ebb1558
iaine <iain.emsley@kellogg.ox.ac.uk>
2024-03-31 20:50:28 +0100

    Added set up

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='agents',
+      version='1.0',
+      description='Create chuck agents',
+      author='Iain Emsley',
+      author_email='iain.emsley@warwick.ac.uk',
+      url='iaine.github.io',
+      packages=['agent'],
+     )
